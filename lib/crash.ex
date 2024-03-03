4eece3b0a58724ea6cec1436a7665bcf55be7bec
megobotonoob <megobotonoob@gmail.com>
2024-03-03 21:51:04 +0300

    names

--- lib/crash.ex
+++ lib/crash.ex
@@ -1,37 +1,19 @@
-defmodule Crash.Crashy do
-  @moduledoc """
-  handles messages with :tick and randomly devites by zero
-  """
-  use GenServer
+defmodule Crash do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, [], opts)
-  end
+  use Application
 
-  def init([]) do
-    {:ok, []}
-  end
-
-  #в какой-то момент вариант с call просто прекращает работу на очередном делении на ноль. Возможно я ловлю дэдлок.
-  #чтобы посмотреть надо поменять внутри Crash.Loop.tick_maker cast на call
-  #  def handle_call(:tick, _from, state) do
-  #    IO.puts "before crashy_function"
-  #    crashy_function()
-  #    {:reply, :ok, state}
-  #  end
-
-  def handle_cast(:tick, state) do
-    crashy_function()
-    {:noreply, state}
-  end
-
-  defp crashy_function() do
-    d = Enum.random(0..10)
-    if d == 0 do
-      IO.puts "Divide by zero"
-    else
-      IO.puts "devide by #{d}"
-    end
-    :erlang.div(1, d)
+  @impl true
+  def start(_type, _args) do
+    Supervisor.start_link(
+      [
+        Crash.Top,
+        Crash.Loop
+      ],
+      strategy: :one_for_one
+    )
   end
 end
+
